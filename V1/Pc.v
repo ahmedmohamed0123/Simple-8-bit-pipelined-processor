506bf0e9caccdbd5c18be45c5be6527cb4a61b14
aya medhat <ayamadhat309@gmail.com>
2025-12-09 20:44:32 +0200

    pc update

--- V1/Pc.v
+++ V1/Pc.v
@@ -6,6 +6,12 @@ module Pc (
 		input wire			load,
 		input wire			imm,
 		input wire	[7:0]	Target,
+		input wire	[7:0]	M0,  //M[0] incase reset
+		input wire	[7:0]	M1,  //M[1] incase interrupt
+		input wire	[7:0]	X,   //SP
+		input wire	[7:0]	B,   // R(b)
+		input wire [1:0]  targer_Sel,
+
 		output reg	[7:0] 	Pc
 
 	);
@@ -19,7 +25,14 @@ always @(posedge clk or negedge rst_n) begin
 
 	else if (en) begin
 
-		if (load) begin
+		if (!load) begin
+			case(targer_Sel)
+              2'b00 :Target=M0;
+              2'b01 :Target=M1;
+              2'b10: Target=X;
+			  2'b11 :Target=B;
+
+			endcase
 		Pc <= Target;
 		end
 		else if (imm) begin
