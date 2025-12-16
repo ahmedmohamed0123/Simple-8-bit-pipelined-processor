a5a9617185c3200a293bd5a36626d015504a6070
Kareem Hassan <kareem.ali04@eng-st.cu.edu.eg>
2025-12-16 09:23:23 +0200

    Fixing Forwarding Unit

--- V4/Forwarding_unit.v
+++ V4/Forwarding_unit.v
@@ -1,35 +1,40 @@
 module forward_unit 
 (
-   input wire  W_E_R_next, // write enable for the current cycle ( o/p from pipeline  after fetch ) to check write in register
-   input wire [1:0] R_ADD_A_next , // read reg A for the current cycle ( o/p from pipeline  after fetch ) 
-   input wire [1:0]  R_ADD_B_next, // read reg B  for the current cycle ( o/p from pipeline  after fetch )
-   input wire [1:0] W_add_last, // write addrees for the last cycle
-   input wire W_E_M_last, // to check there is load data from memory if it low
-   output reg [1:0] forward_A, // to forward data for port A for ALU
-   output reg [1:0] forward_B // to forward data for port  B for ALU
+   input wire       W_E_R_previous,       // write enable for the previous instruction to check write in register (No Write -> No Hazard)
+   input wire [1:0] R_ADD_A_current,      // read address reg A for the current instruction (Source register)
+   input wire [1:0] R_ADD_B_current,      // read address reg B for the current instruction (Source register)
+   input wire [1:0] W_add_previous,       // write address for the previous instruction (destination register)
+   input wire [2:0] w_Data_S_R_previous;  // reg write data selection line for the previous instruction (to check if data is from ALU or Memory) (0: Mem, 1: ALU out , 2: SP, 3: INPUT, 4: IMM)
 
+   output reg [1:0] forward_A,            // to forward data for port A for ALU (ALU's A port's MUX select line)
+   output reg [1:0] forward_B             // to forward data for port B for ALU (ALU's B port's MUX select line)
 );
+
 always @(*) begin 
-    //ideal case
-    forward_A= 2'b00;
-    forward_B=2'b00;
-    // execution hazard (Dependency RAW) (Arithmetic or logical then arithmetic or logical ADD r1.r2 -> ADD r1,r3)
-    if(W_E_R_next && (R_ADD_A_next!=2'b11) && (W_add_last==R_ADD_A_next))
-    forward_A=2'b01;
+   //ideal case
+   forward_A = 2'b00;
+   forward_B = 2'b00;
 
-    if(W_E_R_next && (R_ADD_B_next!=2'b11) && (W_add_last==R_ADD_B_next))
-    forward_B=2'b01;
-
-    // Memory hazard (Dependency RAW) (L_type then arithmetic or logical LDI/LDD/LDM r1,r2(0) -> ADD r1,r3)
-      if(W_E_R_next && !(W_E_M_last) && (R_ADD_A_next!=2'b11) && (W_add_last==R_ADD_A_next))
-      forward_A=2'b10;
-
-       if(W_E_R_next && !(W_E_M_last) && (R_ADD_B_next!=2'b11) && (W_add_last==R_ADD_B_next))
-      forward_B=2'b10;
+   if (W_E_R_previous && (R_ADD_A_current != 2'b11) && (W_add_previous == R_ADD_A_current)) begin
+      if (w_Data_S_R_previous == 0)
+      // Memory hazard (Dependency RAW) (Load then arithmetic or logical LOAD r1 -> ADD r2,r1)
+         forward_A = 2'b10;
+      else if (w_Data_S_R_previous == 1)
+      // Execution hazard (Dependency RAW) (Arithmetic or logical then arithmetic or logical ADD r1.r2 -> ADD r3,r1)
+         forward_A = 2'b01;
+   end
+         
+   if (W_E_R_previous && (R_ADD_B_current != 2'b11) && (W_add_previous == R_ADD_B_current)) begin
+      if (w_Data_S_R_previous == 0)
+         forward_B = 2'b10;
+      else if (w_Data_S_R_previous == 1)
+         forward_B = 2'b01;
+   end
 
 end
 
-endmodule 
+endmodule
+
 /* ALU has mux for 3 inputs now
 00: output from reg file
 01: output from alu 
--- V4/Hazard_Unit.v
+++ V4/Hazard_Unit.v
@@ -1,7 +1,11 @@
+/*The normal code flow is incrementing the PC, but in case of taken branches, jumps, calls, returns, and interrupts,
+the PC needs to be updated to a different address. In such cases, the instructions that were fetched after the branch
+instruction but before the new PC is known must be flushed from the pipeline to prevent them from executing incorrectly.*/
+
 module Hazard_Unit (
     input  wire [3:0] OPCODE,        // instruction opcode in EX stage
 	input  wire [1:0] ra_addr,
-	input wire [7:0] ra_data,
+	input  wire [7:0] ra_data,
     input  wire [3:0] CCR,    
     output reg        FLUSH
 );
@@ -10,20 +14,24 @@ always @(*) begin
     FLUSH = 1'b0;
 
     // Conditional branches (JZ, JN, JC, JV) and LOOP
-    if (OPCODE == 4'd9)
-	 begin
-	  if((CCR == 4'b1000 && ra_addr == 2'd0)||(CCR == 4'b0100 && ra_addr == 2'd1)||(CCR == 4'b0010 && ra_addr == 2'd2)||(CCR == 4'b0001 && ra_addr == 2'd3))
-       FLUSH = 1'b1;
-	  else
-	   FLUSH = 1'b0; 
-     end
+    if (OPCODE == 4'd9) begin
+	    if ((ra_addr == 2'd0 && CCR[0] == 1'b1)      //JZ
+         || (ra_addr == 2'd1 && CCR[1] == 1'b1)      //JN
+         || (ra_addr == 2'd2 && CCR[2] == 1'b1)      //JC
+         || (ra_addr == 2'd3 && CCR[3] == 1'b1))     //JV
+            FLUSH = 1'b1;
+	    else
+	        FLUSH = 1'b0; 
+    end
+    
 	//Loop 
-	else if (OPCODE == 4'd10 && ((ra_data-1)!= 0))
-	 FLUSH = 1'b1; 
+	else if (OPCODE == 4'd10 && !CCR[0])
+	    FLUSH = 1'b1; 
      
     // Unconditional control flow (JMP, CALL, RET, RTI)
-    else if (OPCODE == 4'd11 && (ra_addr == 2'd1 || ra_addr == 2'd2 || ra_addr == 2'd3))
-     FLUSH = 1'b1;
+    else if (OPCODE == 4'd11)
+        FLUSH = 1'b1;
+
 end
 
 endmodule
