02f6066f5dd14d8be7aa528656e9cd7bba786608
Amira Salah <amirasalah2062004@gmail.com>
2025-12-19 00:57:01 +0200

    add V1 pipelined

---
+++ risc_V1_PL/Tb.v
@@ -0,0 +1,39 @@
+module TB ();
+	reg 			clk,rst_n;
+	reg 			interrupt; 
+	reg		[7:0]	In_port;
+	wire	[7:0]	Out_port;
+	
+Top T1(
+	.clk(clk),
+	.rst_n(rst_n), 
+	.interrupt(interrupt),
+	.In_port(In_port),
+	.Out_port(Out_port)
+	);
+
+always #10 clk=~clk;
+ 
+ always @(*) begin 
+	if(T1.Opcode==8'hff)  
+		interrupt=1;
+		else 
+		interrupt=0;
+end 
+
+initial begin
+	clk=0;
+	rst_n=1;
+	 repeat(100) begin
+     @(posedge clk) 
+	if(T1.Opcode==8'hff)  
+		interrupt=1;
+		else 
+		interrupt=0;
+	 end
+
+	#400;
+	$stop;
+end 
+
+endmodule
