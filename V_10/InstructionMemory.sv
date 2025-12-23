929ea0c0e197379511ad3e10b20521999c9f0d24
Mahmoud  Matar <mahmoudmater1234@gmail.com>
2025-12-23 13:35:56 +0200

    Add files via upload

---
+++ V_10/InstructionMemory.sv
@@ -0,0 +1,30 @@
+module instr_mem (
+    
+    input  wire  [7:0]  PC,        // PC input (Memory Address)
+    output wire  [7:0]  instr,     // instruction  output
+    output wire  [7:0]  next_byte  // immediate value or ea
+    output wire   [7:0]  M_0,
+    output wire   [7:0]  M_1,
+);
+
+    // 256x8 ROM
+    reg [7:0] mem [0:255];
+    
+
+    assign M_0 =mem[0];
+    assign M_1 =mem[1];
+
+
+    
+    initial begin
+        $readmemh("program.txt", mem);
+    end
+
+    // Synchronous read
+    assign M_0 =mem[0];
+    assign M_1 =mem[1];
+
+        assign instr = mem[PC];
+        assign next_byte = mem[PC + 8'd1];
+
+endmodule
